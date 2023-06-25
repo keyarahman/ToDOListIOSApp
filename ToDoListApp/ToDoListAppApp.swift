639f9b0502cd33c8dd4a647518b4e4cc93adafbe
keyarahman <keya91bd@gmail.com>
2023-06-25 11:24:40 +0600

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by IOU on 25/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
